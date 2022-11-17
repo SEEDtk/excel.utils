55e3a950ee6492920d2b27c269cb413718353dd7
Bruce Parrello <bparrello@figresearch.com>
2022-11-17 08:21:03 -0600

    New distribution helper.

---
+++ src/main/java/org/theseed/excel/utils/Distributor.java
@@ -0,0 +1,132 @@
+/**
+ *
+ */
+package org.theseed.excel.utils;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.theseed.excel.CustomWorkbook;
+import org.theseed.utils.ParseFailureException;
+
+/**
+ * This object creates a distribution spreadsheet.  The constructor passes in a minimum, a maximum, and a number
+ * of buckets.  Each value passed in has a series name and a value between the extremes.  We count the value in
+ * the appropriate bucket.  A method is provided to write the results to an Excel spreadsheet in the form of a
+ * table.  Each row is a bucket and each column is a series name.
+ *
+ * @author Bruce Parrello
+ *
+ */
+public class Distributor {
+
+    // FIELDS
+    /** logging facility */
+    protected static Logger log = LoggerFactory.getLogger(Distributor.class);
+    /** map of series names to bucket arrays */
+    private Map<String, int[]> bucketMap;
+    /** bucket size */
+    private double bucketWidth;
+    /** size of each bucket array */
+    private int nBuckets;
+    /** minimum value */
+    private double minimum;
+    /** recommended display precision for floating-point */
+    private int precision;
+
+    /**
+     * Construct a distributor.
+     *
+     * @param min		minimum possible value
+     * @param max		maximum possible value
+     * @param n			number of desired buckets
+     *
+     * @throws ParseFailureException
+     */
+    public Distributor(double min, double max, int n) throws ParseFailureException {
+        this.nBuckets = n;
+        if (n <= 1)
+            throw new ParseFailureException("Cannot create a distribution with less than 2 buckets.");
+        if (min >= max)
+            throw new ParseFailureException("Minimum of range must be less than maximum.");
+        this.minimum = min;
+        this.bucketWidth = (max - min) / n;
+        // We use a tree map so that the series names are sorted, and because we expect the series count to be small.
+        this.bucketMap = new TreeMap<String, int[]>();
+        // Compute the recommended format.
+        int digits = (int) Math.ceil(Math.log10(Math.abs(max))) + 1;
+        int divisor = (int) Math.ceil(Math.log10(Math.abs(n))) + 1;
+        this.precision = 0;
+        if (digits - divisor < 0) this.precision = divisor - digits;
+    }
+
+    /**
+     * Add a value to a series.
+     *
+     * @param name		name of the series
+     * @param value		value to record
+     */
+    public void addValue(String name, double value) {
+        // We count on Java's habit of initializing all ints to zero.
+        int[] buckets = this.bucketMap.computeIfAbsent(name, x -> new int[this.nBuckets]);
+        int idx = (int) ((value - this.minimum) / this.bucketWidth);
+        // Catch the maximum if it happens.
+        if (idx >= this.nBuckets) idx = this.nBuckets - 1;
+        // Count the value.
+        buckets[idx]++;
+    }
+
+    /**
+     * Get the bucket array for a series.
+     *
+     * @param name		name of the series
+     *
+     * @return the distribution counts for the named series, or NULL if it does not exist
+     */
+    public int[] getBuckets(String name) {
+        return this.bucketMap.get(name);
+    }
+
+    /**
+     * @return the minimum value for a bucket
+     *
+     * @param idx	bucket index
+     */
+    protected double getLower(int idx) {
+        return this.minimum + idx * this.bucketWidth;
+    }
+
+    /**
+     * Save a spreadsheet for this distribution.
+     *
+     * @param outFile	name of the file in which to store the spreadsheet
+     */
+    public void save(File outFile) {
+        try (CustomWorkbook workbook = CustomWorkbook.create(outFile)) {
+            log.info("Saving distribution data to {}.", outFile);
+            workbook.addSheet("Distribution", true);
+            workbook.setPrecision(this.precision);
+            // Get an ordered list of the buckets.
+            var names = this.bucketMap.keySet();
+            var bucketList = names.stream().map(x -> this.bucketMap.get(x)).collect(Collectors.toList());
+            // Create the header list.
+            List<String> headers = new ArrayList<String>(nBuckets + 1);
+            headers.add("bucket_min");
+            headers.addAll(names);
+            workbook.setHeaders(headers);
+            // Now loop through the rows (one per bucket), filling in the cells.
+            for (int idx = 0; idx < this.nBuckets; idx++) {
+                workbook.addRow();
+                workbook.storeCell(this.getLower(idx));
+                for (int[] buckets : bucketList)
+                    workbook.storeCell(buckets[idx]);
+            }
+        }
+    }
+}
---
+++ src/test/java/org/theseed/excel/utils/TestDistributor.java
@@ -0,0 +1,40 @@
+/**
+ *
+ */
+package org.theseed.excel.utils;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import java.io.File;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+import org.theseed.utils.ParseFailureException;
+
+/**
+ *
+ * @author Bruce Parrello
+ *
+ */
+class TestDistributor {
+
+    public static double[] series1 = new double[] { 0.01, 0.02, 0.03, 0.11, 0.12, 0.13, 0.14, 0.21, 0.22, 0.23, 0.24, 0.25,
+            0.31, 0.32, 0.33, 0.34, 0.35, 0.36, 0.71, 0.72, 0.73, 0.74, 0.81, 0.82, 0.91 };
+    public static double[] series2 = new double[] { 0.0, 0.11, 0.21, 0.31, 0.41, 0.51, 0.61, 0.71, 0.81, 0.91, 1.0 };
+    public static int[] series1Buckets = new int[] { 3, 4, 5, 6, 0, 0, 0, 4, 2, 1 };
+    public static int[] series2Buckets = new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 2 };
+
+    @Test
+    void test() throws ParseFailureException {
+        Distributor bucketMap = new Distributor(0.0, 1.0, 10);
+        Arrays.stream(series1).forEach(x -> bucketMap.addValue("series1", x));
+        Arrays.stream(series2).forEach(x -> bucketMap.addValue("series2", x));
+        assertThat(bucketMap.getBuckets("series3"), nullValue());
+        assertThat(bucketMap.getBuckets("series2"), equalTo(series2Buckets));
+        assertThat(bucketMap.getBuckets("series1"), equalTo(series1Buckets));
+        File outFile = new File("data", "buckets.xlsx");
+        bucketMap.save(outFile);
+    }
+
+}
